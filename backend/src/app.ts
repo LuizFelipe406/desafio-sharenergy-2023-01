50a9e603baa07661524893862d7d7d0ff9cbc248
LuizFelipe406 <luizfpereira406@gmail.com>
2023-01-11 10:04:04 -0300

    feat: Adiciona app.ts e server.ts

---
+++ backend/src/app.ts
@@ -0,0 +1,35 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+
+export default class App {
+  public app: express.Express;
+
+  constructor() {
+    this.app = express();
+
+    this.config();
+
+    this.configRoutes();
+  }
+
+  private config(): void {
+    this.app.use(express.json());
+    this.app.use(
+      cors({
+        origin: process.env.FRONTEND_URL || "http://localhost:3000",
+      })
+    );
+  }
+
+  private configRoutes(): void {
+    this.app.get("/", (req, res) => res.json({ ok: true }));
+  }
+
+  public start(PORT: string): void {
+    this.app.listen(PORT, () => console.log(`Running on port ${PORT}`));
+  }
+}
+
+// A execução dos testes de cobertura depende dessa exportação
+export const { app } = new App();
