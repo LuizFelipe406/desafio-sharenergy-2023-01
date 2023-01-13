430ed0b6579e285dc59fdb6b6ce9448c5a6eaaac
LuizFelipe406 <luizfpereira406@gmail.com>
2023-01-13 16:01:12 -0300

    Feat: Adiciona implementação da Pagina de Login

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,9 +1,90 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import useLocalStorage from '../hooks/useLocalStorage';
+import requestApi from '../utils/requestApi';
 
 function Login() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [rememberMe, setRememberMe] = useLocalStorage('rememberMe', false);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (rememberMe) {
+      setUsername(localStorage.getItem('username') || '');
+      setPassword(localStorage.getItem('password') || '');
+      login();
+    }
+  }, [])
+
+  const handleUsernameChange = ({
+    target,
+  }: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = target;
+    setUsername(value);
+    if (rememberMe) {
+      localStorage.setItem('username', value);
+    }
+  };
+
+  const handlePasswordChange = ({
+    target,
+  }: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = target;
+    setPassword(value);
+    if (rememberMe) {
+      localStorage.setItem('password', value);
+    }
+  };
+
+  const handleRememberMeChange = ({
+    target,
+  }: React.ChangeEvent<HTMLInputElement>) => {
+    const { checked } = target;
+    setRememberMe(checked);
+    if (checked === true) {
+      localStorage.setItem('password', password);
+      localStorage.setItem('username', username);
+    } else {
+      localStorage.removeItem('password');
+      localStorage.removeItem('username');
+    }
+  };
+
+  const login = async () => {
+    const { status, data } = await requestApi('POST', '/login', { username, password });
+    if (status === 200) {
+      localStorage.setItem('token', data);
+      navigate('/clientList');
+    }
+  }
+
   return (
     <div>
-      pagina de login
+      <input
+        type="text"
+        value={ username }
+        onChange={ handleUsernameChange }
+      />
+      <input
+        type="password"
+        value={ password }
+        onChange={ handlePasswordChange }
+      />
+      <button
+        type="button"
+        onClick={ login }
+        disabled={ username.length === 0 || password.length === 0 }
+      >
+        Entrar
+      </button>
+      <input
+        id="rememberMe"
+        type="checkbox"
+        checked={ rememberMe }
+        onChange={ handleRememberMeChange }
+      />
+      <label htmlFor="rememberMe">Remember Me</label>
     </div>
   )
 }
---
+++ frontend/src/utils/requestApi.ts
@@ -0,0 +1,30 @@
+import axios from "axios";
+
+type headers = {
+  authorization: string;
+};
+
+const HOST = process.env.REACT_APP_API_HOST || "localhost:3001";
+const PROTOCOL = process.env.REACT_APP_API_PROTOCOL || "http";
+
+const request = axios.create({
+  baseURL: `${PROTOCOL}://${HOST}/`,
+  timeout: 10000,
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+  },
+});
+
+const requestApi = async (
+  method: string,
+  endpoint: string,
+  body: object,
+  headers?: headers
+) =>
+  request
+    .request({ method, url: endpoint, data: body, headers })
+    .then(({ status, data }) => ({ status, data }))
+    .catch((error) => error.toJSON());
+
+export default requestApi;
