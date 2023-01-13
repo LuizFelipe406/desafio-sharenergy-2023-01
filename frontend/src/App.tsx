02594bff7ba59cbb435c18692481d64534f7d13b
LuizFelipe406 <luizfpereira406@gmail.com>
2023-01-13 15:22:42 -0300

    Feat: Setup React Router Dom

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,13 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Login from './pages/Login';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<Login />}/>
+      </Routes>
     </div>
   );
 }
