8ad82243355fa02381b44039e817778f58f5b701
LuizFelipe406 <luizfpereira406@gmail.com>
2023-01-17 09:20:28 -0300

    style: Adiciona estilização para pagina httpCat

--- frontend/src/pages/HttpCat.tsx
+++ frontend/src/pages/HttpCat.tsx
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
+import { BsSearch } from 'react-icons/bs';
 import SideBar from '../components/SideBar';
 
 function HttpCat () {
-  const [statusCode, setStatusCode] = useState('0');
+  const [statusCode, setStatusCode] = useState('');
   const [catImage, setCatImage] = useState('');
 
   const handleInputChange = ({
@@ -13,26 +14,34 @@ function HttpCat () {
   };
 
   const requestCat = () => {
-    setCatImage(`https://http.cat/${statusCode}`)
+    if (statusCode.length === 0) {
+      setCatImage('https://http.cat/404')
+    } else {
+      setCatImage(`https://http.cat/${statusCode}`)
+    }
+    setStatusCode('');
   }
 
   return (
-    <div>
+    <div className="bg-gradient-to-tr from-dcream to-cream font-plexSans">
       <SideBar />
-      <div className="flex items-center w-screen justify-center">
-        <img src={catImage} alt="cat image" />
-        <input
-          type="number"
-          onChange={ handleInputChange }
-          value={ statusCode }
-          placeholder="Digite um Status Code"
-        />
-        <button
-          type="button"
-          onClick={requestCat}
-        >
-          pesquisar
-        </button>
+      <div className="h-screen w-screen flex flex-col items-center justify-center">
+        <div className="h-11 flex items-center justify-center mb-5 w-96 ml-10">
+          <input
+            className="h-full px-4 w-full rounded-l-lg shadow-md focus:outline-none"
+            onChange={ handleInputChange }
+            value={ statusCode }
+            placeholder="Digite um Status Code"
+          />
+          <button
+            className="bg-green h-full px-3 rounded-r-lg shadow-md mr-8"
+            type="button"
+            onClick={requestCat}
+          >
+            {<BsSearch />}
+          </button>
+        </div>
+        <img src={catImage} alt="cat image" className={`rounded-3xl h-3/6 mt-16 ${catImage.length === 0 ? 'invisible' : ''}`} />
       </div>
     </div>
   )
